b796afc91b558ec4a2136f2e48a2041fb99911f8
dadeval <dadeval@301e733a-ad6d-11dd-8748-9355f1163679>
2008-11-28 11:11:20 +0000

    added water level button

---
+++ AvalonUgh/AvalonUgh.Code/Editor/WaterLevelSelector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib;
+using System.Windows.Controls;
+using ScriptCoreLib.Shared.Avalon.Extensions;
+using ScriptCoreLib.Shared.Lambda;
+using AvalonUgh.Assets.Shared;
+namespace AvalonUgh.Code.Editor
+{
+	[Script]
+	public class WaterLevelSelector
+	{
+		public static readonly View.SelectorInfo[] Sizes =
+			new View.SelectorInfo[]
+			{
+				new Size_Vertical()
+			};
+
+
+
+		[Script]
+		internal class Size_Vertical : View.SelectorInfo
+		{
+			public Size_Vertical()
+			{
+				// we are setting a width that should be greater than the view width
+				// we might add a support for -1 to expand to to view later
+				this.Width = 1000;
+
+				this.Height = 4;
+			}
+
+			public override void CreateTo(Level Level, View.SelectorPosition Position)
+			{
+				var WaterHeight = PrimitiveTile.Heigth * Level.Map.Height - Position.ContentY;
+
+				Console.WriteLine(
+					"water: " +
+					WaterHeight
+				);
+
+				// the view should listen to this event
+				// and update our water animation at runtime
+				Level.AttributeWater.Value = WaterHeight;
+			}
+
+		}
+
+
+
+
+	}
+}
