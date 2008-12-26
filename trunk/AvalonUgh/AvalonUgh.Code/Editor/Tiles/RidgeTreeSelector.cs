ea643970289eac5b108594726c1f874fc315a662
dadeval <dadeval@301e733a-ad6d-11dd-8748-9355f1163679>
2008-12-26 19:55:24 +0000

    adding ridge tree

---
+++ trunk/AvalonUgh/AvalonUgh.Code/Editor/Tiles/RidgeTreeSelector.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using ScriptCoreLib;
+using ScriptCoreLib.Shared.Avalon.Extensions;
+using ScriptCoreLib.Shared.Lambda;
+using AvalonUgh.Assets.Shared;
+using AvalonUgh.Assets.Avalon;
+
+namespace AvalonUgh.Code.Editor.Tiles
+{
+	[Script]
+	public class RidgeTreeSelector : SelectorBase
+	{
+		public const string Identifier = "T";
+
+		public readonly View.SelectorInfo 
+			Size_1x1 = new Size_Generic(1, 1, 3);
+			//Size_2x1 = new Size_Generic(2, 1, 1),
+			//Size_2x2 = new Size_Generic(2, 2, 3),
+			//Size_2x3 = new Size_Generic(2, 3, 2),
+			//Size_3x2 = new Size_Generic(3, 2, 2),
+			//Size_3x3 = new Size_Generic(3, 3, 1),
+			//Size_4x4,
+			//Size_5x5,
+			//Size_5x4;
+
+
+
+
+		public RidgeTreeSelector()
+		{
+			this.ToolbarImage =
+				new NameFormat
+				{
+					Path = Assets.Shared.KnownAssets.Path.Tiles,
+					Name = "ridgetree",
+					Index = 500,
+					Extension = "png"
+				};
+
+			//this.Size_5x5 =
+			//    new TileSelector.Composite(5, 5,
+			//        (Level, Position) =>
+			//        {
+			//            this.Size_1x1.CreateTo(Level, Position[2, 2]);
+			//            this.Size_3x2.CreateTo(Level, Position[0, 0]);
+			//            this.Size_2x3.CreateTo(Level, Position[0, 2]);
+			//            this.Size_2x3.CreateTo(Level, Position[3, 0]);
+			//            this.Size_3x2.CreateTo(Level, Position[2, 3]);
+			//        }
+			//    );
+
+			//this.Size_5x4 =
+			//    new TileSelector.Composite(5, 4,
+			//        (Level, Position) =>
+			//        {
+			//            this.Size_2x2.CreateTo(Level, Position[0, 0]);
+			//            this.Size_3x2.CreateTo(Level, Position[2, 0]);
+			//            this.Size_3x2.CreateTo(Level, Position[0, 2]);
+			//            this.Size_2x2.CreateTo(Level, Position[3, 2]);
+			//        }
+			//    );
+
+			//this.Size_4x4 =
+			//    new TileSelector.Composite(4, 4,
+			//        (Level, Position) =>
+			//        {
+			//            this.Size_2x1.CreateTo(Level, Position[0, 0]);
+			//            this.Size_2x3.CreateTo(Level, Position[0, 1]);
+			//            this.Size_2x3.CreateTo(Level, Position[2, 0]);
+			//            this.Size_2x1.CreateTo(Level, Position[2, 3]);
+			//        }
+			//    );
+
+			this.Sizes =
+				new View.SelectorInfo[]
+				{
+					Size_1x1,
+					//Size_2x1,
+					//Size_2x2,
+					//Size_2x3,
+					//Size_3x2,
+					//Size_3x3,
+
+					//Size_4x4,
+					//Size_5x4,
+					//Size_5x5
+				};
+		}
+
+		
+
+		[Script]
+		private class Size_Generic : TileSelector.Named
+		{
+
+			public Size_Generic(int x, int y, int variations)
+				: base(x, y, variations, "ridgetree")
+			{
+			
+			}
+
+			public override void CreateTo(Level Level, View.SelectorPosition Position)
+			{
+				Name.Index = (Name.Index + 1) % Name.IndexCount;
+
+			
+
+				RemovePlatforms(this, Level, Position);
+				RemoveEntities(this, Level, Position);
+
+				//var u = new Ridge(Level, this)
+				//{
+				//    Position = Position,
+				//    Image = ToImage(Level, Position)
+				//};
+
+				//Level.KnownRidges.Add(u);
+
+
+
+				//{
+				//    // bridge connected only by the corner will also have legs!
+				//    var TriggerPosition = Position[-1, -1];
+
+				//    var o_trigger = Obstacle.Of(TriggerPosition, Level.Zoom, 1, 1);
+
+				//    var trigger = Level.KnownBridges.FirstOrDefault(k => k.ToObstacle().Intersects(o_trigger));
+
+				//    if (trigger != null)
+				//    {
+				//        Level.KnownBridges.Remove(trigger);
+
+				//        new BridgeSelector().Size_1x1.CreateTo(Level, TriggerPosition);
+				//    }
+				//}
+
+				//{
+				//    // bridge connected only by the corner will also have legs!
+				//    var TriggerPosition = Position[this.PrimitiveTileCountX, -1];
+
+				//    var o_trigger = Obstacle.Of(TriggerPosition, Level.Zoom, 1, 1);
+
+				//    var trigger = Level.KnownBridges.FirstOrDefault(k => k.ToObstacle().Intersects(o_trigger));
+
+				//    if (trigger != null)
+				//    {
+				//        Level.KnownBridges.Remove(trigger);
+
+				//        new BridgeSelector().Size_1x1.CreateTo(Level, TriggerPosition);
+				//    }
+				//}
+			}
+		}
+
+		public static void AttachToLevel(ASCIIImage.Entry Position, ASCIITileSizeInfo Tile, Level Level)
+		{
+			//var Selector = new RidgeSelector().Sizes.SingleOrDefault(
+			//    k => k.Equals(Tile)
+			//);
+
+			//if (Selector == null)
+			//{
+			//    Console.WriteLine(
+			//        new { InvalidSize = new { Tile.Width, Tile.Height }, Identifier, Position.X, Position.Y }.ToString()
+			//    );
+
+			//    return;
+			//}
+
+			//Selector.CreateTo(Level,
+			//    new View.SelectorPosition
+			//    {
+			//        ContentX = Position.X * PrimitiveTile.Width,
+			//        ContentY = Position.Y * PrimitiveTile.Heigth,
+			//    }
+			//);
+		}
+	}
+}
