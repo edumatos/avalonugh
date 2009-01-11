75ef51fb577744b1bd2e24908311475585ab9427
dadeval <dadeval@301e733a-ad6d-11dd-8748-9355f1163679>
2009-01-11 16:36:38 +0000

    added social links

---
+++ trunk/AvalonUgh/AvalonUgh.Promotion/Info.Flickr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AvalonUgh.Promotion
+{
+	public static partial class Info
+	{
+		public static class Flickr
+		{
+
+			public const string ScreenshotURL = "http://farm4.static.flickr.com/3114/3188232436_03aa393eaf_o.png";
+			public const string ScreenshotSmallURL = "http://farm4.static.flickr.com/3114/3188232436_de3fa23f44_m.jpg";
+		}
+	}
+}
---
+++ trunk/AvalonUgh/AvalonUgh.Promotion/Info.GoogleGadget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AvalonUgh.Promotion
+{
+	public static partial class Info
+	{
+		public static class GoogleGadget
+		{
+
+			public const string Category1 = "lifestyle";
+			public const string Category2 = "funandgames";
+
+			public const string Module = "http://hosting.gmodules.com/ig/gadgets/file/112091410969506928037/avalon-ugh.xml";
+
+			public const string AddLink = "http://fusion.google.com/ig/add?synd=open&source=ggyp&moduleurl=" + Module;
+
+			public const string AddImage = "http://www.gmodules.com/ig/images/plus_google.gif";
+			public const string AddImageWide = "http://buttons.googlesyndication.com/fusion/add.gif";
+
+			public const string AddToWebPageLink = "http://www.google.com/ig/creator?url=http%3A%2F%2Fhosting.gmodules.com%2Fig%2Fgadgets%2Ffile%2F112091410969506928037%2Favalon-ugh.xml";
+			public const string AddToYourWebPageLink = "http://www.gmodules.com/ig/creator?synd=open&hl=en&url=http://hosting.gmodules.com/ig/gadgets/file/112091410969506928037/avalon-ugh.xml";
+
+		}
+	}
+}
